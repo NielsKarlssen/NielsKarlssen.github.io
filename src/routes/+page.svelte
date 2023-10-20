90b52552b767ee38d50057cfa5c7501c4b2c947c
BigHenrik55 <melhol678@gmail.com>
2023-10-20 11:03:07 +0200

    klar med start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,37 @@
 
-<h1>
-  Välkommen till min hemsida!
+<h1 class="hidden">
+  Hjälp!
+
+
+
+ 
 </h1>
+<p id="inspo">
+  "The only way to learn a new programming language is by writing
+  programs in it." - Dennis Ritchie, creator of the C programming language.
+  </p>
+  <p style="background-color:pink">
+  "Every great developer you know got there by solving problems they
+  were unqualified to solve until they actually did it." - Patrick McKenzie,
+  software engineer and entrepreneur.
+  </p>
+<h2 class="flex-container">En suspekt karl är på fri fot</h2>
 
-<h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
+<h3 class="name">-Niels Karlssen</h3>
 
-<h3 >Förnamn Efternamn</h3>
+<h4 class="grid-container">hej</h4>
 
+
+  <style>
+    #inspo{
+font-family: courier;
+font-size: 160%;}
+  .name{
+    color:purple;
+    font-family:'Times New Roman', Times, serif;
+  }
+  h1, h2, h3, p {
+display: inline;
+  }
+
+  </style>
