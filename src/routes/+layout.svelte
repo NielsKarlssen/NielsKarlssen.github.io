ccb86422aa1bb0d7ae49dc0c67eee17b97ec6110
BigHenrik55 <melhol678@gmail.com>
2023-12-01 11:00:58 +0100

    Klar med register

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
   <a href="/memory">Memory</a>
   <a href="/telltale">Telltale</a>
   <a href="/photoapp">Photo App</a>
+  <a href="/register">register</a>
 
   <a
     target="_blank" rel="noreferrer"
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+    let firstname = ""
+    let lastname = ""
+function handleSubmit() {
+// Perform form submission logic here
+console.log("" + firstname +" "+ lastname + "You have succesfully logged in!");
+}
+</script>
+
+
+<p>New to this website? <a
+
+    href="/register">register</a></p>
+
+<main>
+
+    <h1>Log in</h1>
+
+</main>
+
+
+
+
+
+
+
+
+<form on:submit|preventDefault={handleSubmit}>
+
+
+    <label for="email">Email:</label>
+    <input id="email" type = "email"/>
+
+    <label for="password">Password:</label>
+    <input id="password" type="password"/>
+
+<button type="submit">Register</button>
+
+</form>
+
+
+
+
+<style>
+
+form {grid-template-columns: repeat(2, minmax(200px, 1fr));}
+
+    form{display:inline-grid;}
+
+
+
+</style>
